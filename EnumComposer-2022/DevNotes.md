b96066ee4b21cdbe0d78715355caf32301ac9de4
Victror Cherniavsky <Victor.Cherniavsky@fsrao.ca>
2021-11-14 10:38:14 -0500

    dfds

---
+++ EnumComposer-2022/DevNotes.md
@@ -0,0 +1,2 @@
+﻿https://docs.microsoft.com/en-us/visualstudio/extensibility/walkthrough-publishing-a-visual-studio-extension?view=vs-2022
+https://docs.microsoft.com/en-us/azure/devops/extend/overview?view=azure-devops&preserve-view=true&viewFallbackFrom=vsts
---
+++ Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("FSRAO")]
+[assembly: AssemblyProduct("Tests")]
+[assembly: AssemblyCopyright("Copyright © FSRAO 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("67c33713-3aa3-4267-a86c-a3d0bf69a257")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
